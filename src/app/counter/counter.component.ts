3f413a31f9f1508d57eac322b9951d845d6e6356
David <david38ing@gmail.com>
2023-12-16 13:09:42 -0600

    02

---
+++ src/app/counter/counter.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-counter',
+  templateUrl: './counter.component.html',
+})
+
+export class CounterComponent {
+  public title: string = 'App en angular';
+  public counter: number = 10;
+
+  increaseBy(value: number): void { this.counter += value; }
+  reset(): void { this.counter = 10 }
+}
