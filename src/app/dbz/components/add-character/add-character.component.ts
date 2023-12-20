779401debde5cda2a6871cd01212a93065c9454f
David <david38ing@gmail.com>
2023-12-20 11:59:11 -0600

    input y ouutput

--- src/app/dbz/components/add-character/add-character.component.ts
+++ src/app/dbz/components/add-character/add-character.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { Character } from '../../interfaces/character.interface';
 
 @Component({
   selector: 'app-dbz-add-character',
   templateUrl: './add-character.component.html',
-  styleUrl: './add-character.component.css'
+  styleUrl: './add-character.component.css',
 })
 export class AddCharacterComponent {
+  public character: Character = {
+    name: '',
+    power: 0,
+  };
 
+  @Output()
+  public onNewCharacter: EventEmitter<Character> = new EventEmitter();
+  emiitCharacther(): void {
+    console.log(this.character);
+    if (this.character.name.length === 0) return;
+    this.onNewCharacter.emit(this.character);
+    this.character.name = '';
+    this.character.power = 0;
+  }
 }
